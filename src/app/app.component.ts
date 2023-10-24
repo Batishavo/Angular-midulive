da54ed5402288bb3bdfe9cf299e41943a3868031
batishavo <romero.martinez.carlosraul@gmail.com>
2023-10-24 14:45:20 -0600

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'midudev-app';
+  changeTitle(){
+    this.title = 'changed';
+  }
 }
---
+++ src/app/models/product.models.ts
@@ -0,0 +1,6 @@
+export interface Prodcuct{
+    id:number;
+    title:string;
+    price:number;
+    images:string[];
+}
